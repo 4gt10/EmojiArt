23e0584a8c84cc8a306dabe37049375ef1316f17
Artur Chernov <forgot10soul@gmail.com>
2024-01-14 20:43:11 +0700

    Prominent palette chooser

--- EmojiArt/EmojiArt/EmojiPalette/EmojiPaletteView.swift
+++ EmojiArt/EmojiArt/EmojiPalette/EmojiPaletteView.swift
@@ -21,6 +21,7 @@ struct EmojiPaletteView: View {
                         viewModel.removePalette()
                     }
                 }
+                .animation(nil, value: viewModel.cursor)
             palette(at: viewModel.cursor)
                 .id(viewModel.palettes[viewModel.cursor].id)
                 .transition(.asymmetric(insertion: .move(edge: .bottom), removal: .move(edge: .top)))
@@ -32,6 +33,9 @@ struct EmojiPaletteView: View {
         AnimatedActionButton(viewModel.palettes[viewModel.cursor].name, systemImage: "paintpalette") {
             viewModel.goNextPalette()
         }
+        .padding(.horizontal)
+        .foregroundStyle(.foreground)
+        .background(.background, in: RoundedRectangle(cornerRadius: 10))
     }
     
     private func palette(at index: Int) -> some View {
